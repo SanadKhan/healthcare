e29af4b6dbe4d7ce12cb26cd600357daa82caba7
SanadKhan <khansanad72485@gmail.com>
2024-07-19 23:08:05 +0530

    healthcare 1907

--- app/page.tsx
+++ app/page.tsx
@@ -16,21 +16,20 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
           <PatientForm />
-          <div className="flex justify-between">
-         
-            <p className="justify-items-end text-dark-600 xl:text-left"> 2024 CarePulse</p>
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
             <Link href="/?admin=true" className="text-green-500">
-            Admin
+              Admin
             </Link>
           </div>
         </div>
       </section>
-      <Image 
+      <Image
         src={"/assets/images/onboarding-img.png"}
-        height={500}
-        width={500}
+        height={1000}
+        width={1000}
         alt="onboarding"
-        className="w-[50%]"
+        className="side-img max-w-[50%]"
       />
     </div>
   );
--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,8 +1,107 @@
-import React from 'react'
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+  fullname: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email({ message: "Invalid Email " }),
+  phone: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
 
 const PatientForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullname: "",
+      email: "",
+      phone: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>PatientForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+        <section className="mb-12 space-y-4">
+          <h1 className='header'>Hi there, ...</h1>
+          <p className='text-dark-700'>Get Started with Appointments.</p>
+        </section>
+        <FormField
+          control={form.control}
+          name="fullname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full name</FormLabel>
+              <FormControl>
+                <Input placeholder="ex. Adam" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email address</FormLabel>
+              <FormControl>
+                <Input placeholder="example@demo.com" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone number</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="989456784" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                      This is your public display name.
+                    </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
